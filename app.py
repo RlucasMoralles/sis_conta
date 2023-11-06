264cb54d617fd8d9e266424345a465031b0cfd3e
Israel Castro <94873358+IsraelCtr@users.noreply.github.com>
2023-11-05 23:05:40 -0300

    asasas

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from model import AppBD
-from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crudgestao.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+
 
 banco = AppBD()
 banco.criarTabelas()
